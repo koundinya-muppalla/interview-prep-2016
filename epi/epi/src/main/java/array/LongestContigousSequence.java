47d9a3c0ad2f1bf28c9342b7c258cf5a5c0a836e
koundinya <mnv.koundinya@gmail.com>
2017-01-07 19:42:44 -0800

    Longest Contiguous SubArray

---
+++ epi/epi/src/main/java/array/LongestContigousSequence.java
@@ -0,0 +1,30 @@
+package array;
+
+public class LongestContigousSequence {
+
+	public static int longestContigousSequence(int[] arr) {
+		if(arr == null) {
+			return 0;
+		}
+
+		if(arr.length <= 1) { 
+			return arr.length;
+		}
+
+		int start = 0, end = 1, max = 1;
+		while(end < arr.length) {
+			if(arr[end] == arr[end - 1] + 1) {
+				max = Math.max(max, end - start + 1);
+			} else {
+				start = end;
+			}
+			end++;
+		}
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,5,6,7,8};
+		System.out.println(longestContigousSequence(arr));
+	}
+}
---
+++ epi/epi/src/main/java/stacksandqueue/QueueMax.java
@@ -0,0 +1,40 @@
+package stacksandqueue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class QueueMax {
+
+	private Queue<Integer> elements;
+	
+	private Deque<Integer> maxQueue;
+	
+	int max = -1;
+	
+	public QueueMax() {
+		this.elements = new LinkedList<Integer>();
+		this.maxQueue = new LinkedList<Integer>();
+	}
+	
+	public void offer(int val) {
+		this.elements.offer(val);
+		while(!maxQueue.isEmpty() && maxQueue.peekLast() < val) {
+			maxQueue.removeLast();
+		}
+		maxQueue.addLast(val);
+	}
+	
+	public int poll() {
+		int val = this.elements.poll();
+		if(maxQueue.peekFirst() == val) {
+			maxQueue.pollFirst();
+		}
+		return val;
+	}
+	
+	public int max() {
+		return maxQueue.peekFirst();
+	}
+}
