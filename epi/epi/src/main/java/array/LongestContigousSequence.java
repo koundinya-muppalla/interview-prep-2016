e0c7291278301009e538679cdde54de09c8a38b3
koundinya <mnv.koundinya@gmail.com>
2017-01-07 19:43:36 -0800

    Longest Contiguous SubArray

--- epi/epi/src/main/java/array/LongestContigousSequence.java
+++ epi/epi/src/main/java/array/LongestContigousSequence.java
@@ -22,7 +22,7 @@ public class LongestContigousSequence {
 		}
 		return max;
 	}
-	
+
 	public static void main(String[] args) {
 		int[] arr = {1,2,3,5,6,7,8};
 		System.out.println(longestContigousSequence(arr));
