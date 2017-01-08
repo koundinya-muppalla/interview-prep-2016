f07ee15cb504657691efcd5664bf4236566c07f3
koundinya <mnv.koundinya@gmail.com>
2017-01-07 19:49:19 -0800

    Longest Contiguous SubArray

--- epi/epi/src/main/java/array/LongestContigousSequence.java
+++ epi/epi/src/main/java/array/LongestContigousSequence.java
@@ -2,29 +2,29 @@ package array;
 
 public class LongestContigousSequence {
 
-	public static int longestContigousSequence(int[] arr) {
-		if(arr == null) {
-			return 0;
-		}
+    public static int longestContigousSequence(int[] arr) {
+        if(arr == null) {
+            return 0;
+        }
 
-		if(arr.length <= 1) { 
-			return arr.length;
-		}
+        if(arr.length <= 1) { 
+            return arr.length;
+        }
 
-		int start = 0, end = 1, max = 1;
-		while(end < arr.length) {
-			if(arr[end] == arr[end - 1] + 1) {
-				max = Math.max(max, end - start + 1);
-			} else {
-				start = end;
-			}
-			end++;
-		}
-		return max;
-	}
+        int start = 0, end = 1, max = 1;
+        while(end < arr.length) {
+            if(arr[end] == arr[end - 1] + 1) {
+                max = Math.max(max, end - start + 1);
+            } else {
+                start = end;
+            }
+            end++;
+        }
+        return max;
+    }
 
-	public static void main(String[] args) {
-		int[] arr = {1,2,3,5,6,7,8};
-		System.out.println(longestContigousSequence(arr));
-	}
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5,6,7,8};
+        System.out.println(longestContigousSequence(arr));
+    }
 }
