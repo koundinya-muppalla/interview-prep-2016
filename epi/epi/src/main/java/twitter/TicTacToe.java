98bfc4464157e646ab07ba8586fdb3a852aaf356
kmuppallavnera <kmuppalla@vmware.com>
2017-03-03 14:03:46 -0800

    TicTacToe Code review

--- epi/epi/src/main/java/twitter/TicTacToe.java
+++ epi/epi/src/main/java/twitter/TicTacToe.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 // The computer player's behavior/strategy can be replaced by inheriting from the interface below
 // Also, the human player's behavior inherits from the same interface
 // This also makes it easy to modify the game for 2 human players, 2 computer players etc.
-
+// Review this
 interface MoveMethod
 {
     public int move();
