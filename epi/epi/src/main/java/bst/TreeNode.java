7e4f6031536231207626821608487acf24957842
Koundinya <mnv.koundinya@gmail.com>
2018-07-11 18:04:27 -0700

    revert back

--- epi/epi/src/main/java/bst/TreeNode.java
+++ epi/epi/src/main/java/bst/TreeNode.java
@@ -9,7 +9,6 @@ public class TreeNode {
         this.data = data;
         this.left = null;
         this.right = null;
-        this.right = null;
     }
 
     public TreeNode(int data, TreeNode left, TreeNode right) {
